c8ca3de7f7db5faf812b02b030bf8dc1cdb174cc
Guilherme Almeida <glmd832@gmail.com>
2023-08-29 07:34:33 -0300

    design pattern Chain of Responsibility

--- src/TesteImposto.java
+++ src/TesteImposto.java
@@ -1,3 +1,4 @@
+import desconto.CalculadoraDeDescontos;
 import imposto.CalculadoraImpostos;
 import imposto.ISS;
 import orcamento.Orcamento;
@@ -8,8 +9,13 @@ import java.math.BigDecimal;
 public class TesteImposto {
 
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 1);
+        Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+
         CalculadoraImpostos impostos = new CalculadoraImpostos();
-        System.out.println(impostos.calcular(orcamento, new ISS()));
+        System.out.println(impostos.calcular(primeiro, new ISS()));
+
+        CalculadoraDeDescontos descontos = new CalculadoraDeDescontos();
+        System.out.println(descontos.calcular(segundo));
     }
 }
